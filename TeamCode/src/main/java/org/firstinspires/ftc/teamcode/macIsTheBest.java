b57a7337204d5945dea4c503ee157e4321fa0fe0
msmithdr <msmithdr@gmail.com>
2016-10-16 17:05:07 -0400

    Branches?
    Cancers

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/macIsTheBest.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+/**
+ * Created by Mac on 10/16/2016.
+ */
+
+@Autonomous(name = "Sensor: test", group = "Sensor")
+public class macIsTheBest extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        idle();
+    }
+
+}
