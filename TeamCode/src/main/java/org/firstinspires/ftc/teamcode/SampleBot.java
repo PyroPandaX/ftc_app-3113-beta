a834c37247dad4fb696bf5a40d3cf6b1ae5ae96f
efrank48302 <efrank48302@hotmail.com>
2016-11-14 19:15:39 -0500

    a

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SampleBot.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="SampleBot", group="Demo Bot")
+//@Disabled
+public class SampleBot extends OpMode {
+    final static double MOTOR_POWER = 0.2;
+    DcMotor motorRB, motorRF, motorLB, motorLF;
+    //ColorSensor colorSensor;
+    //float hsvValues[] = {0F,0F,0F};
+    //final float values[] = hsvValues;
+    //final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
+    //boolean bPrevState;
+    //boolean bCurrState;
+    //boolean bLedOn;
+    public double right;
+    public double left;
+
+    public SampleBot() {
+    }
+
+    public void init() {
+        //bPrevState = false;
+        //bCurrState = true;
+        //bLedOn = true;
+        motorRB = hardwareMap.dcMotor.get("motor_1");
+        motorRF = hardwareMap.dcMotor.get("motor_2");
+        motorLB = hardwareMap.dcMotor.get("motor_3");
+        motorLF = hardwareMap.dcMotor.get("motor_4");
+
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+        right = gamepad1.right_stick_y;
+        left = -gamepad1.left_stick_y;
+
+        motorRB.setPower(right);
+        motorLB.setPower(left);
+        motorLF.setPower(left);
+        motorRF.setPower(right);
+
+        telemetry.addData("1", "rightPower", String.format("%.22f", right));
+        telemetry.addData("2", "leftPower",  String.format("%.22f", left));
+    }
+
+    @Override
+    public void stop() {}
+}
